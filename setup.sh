2123b1a09065f082d80b0a3f5897ab9a430e3cfd
moiseshbs <moiseshenryb@gmail.com>
2025-04-18 14:30:51 -0300

    Correção na entrada de usuário

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 echo "Iniciando configuração de ambiente..."
 
 echo "Atualizando os pacotes do sistema"
-sudo apt update && sudo apt upgrade
+sudo apt update && sudo apt upgrade -y
 
 
 #configurando java
@@ -21,10 +21,10 @@ if [ $? = 0 ]; #se retorno for igual a 0
         echo "Gostaria de instalar o java? [s/n]" #print no terminal
         read get #variável que guarda resposta do usuário
 
-        if [ \"$get\" == \"s\" ]; #se retorno for igual a s
+        if [ "$get" = "s" ]; #se retorno for igual a s
 
             then #entao
-            sudo apt install openjdk-21-jre -y #executa instalacao do java
+            sudo apt install openjdk-21-jre-headless -y #executa instalacao do java
 
         fi #fecha o 2º if
 
@@ -46,7 +46,7 @@ if [ $? = 0 ]; #se retorno for igual a 0
         echo "Gostaria de instalar o Docker? [s/n]" #print no terminal
         read get #variável que guarda resposta do usuário
 
-        if [ \"$get\" == \"s\" ]; #se retorno for igual a s
+        if [ "$get" = "s" ]; #se retorno for igual a s
 
             then #entao
             sudo apt install docker.io -y #executa instalacao do java
@@ -59,28 +59,4 @@ fi #fecha o 1º if
 echo "Clonando repositório..."
 sudo docker pull moiseshbs/mobilitech:1.0
 
-sudo docker run moiseshbs/mobilitech:1.0
-
-#configurando npm
-# echo "Verificando NodeJS..."
-
-# node --version
-
-# if [ $? = 0 ]; #se retorno for igual a 0
-    
-#     then #entao,
-#         echo "NodeJS instalado" #print no terminal
-
-#     else #se nao,
-#         echo "NodeJS não instalado" #print no terminal
-#         echo "Gostaria de instalar o NodeJS? [s/n]" #print no terminal
-#         read get #variável que guarda resposta do usuário
-
-#         if [ \"$get\" == \"s\" ]; #se retorno for igual a s
-
-#             then #entao
-#             sudo apt install npm -y #executa instalacao do java
-
-#         fi #fecha o 2º if
-
-# fi #fecha o 1º if
+sudo docker run -d moiseshbs/mobilitech:1.0
